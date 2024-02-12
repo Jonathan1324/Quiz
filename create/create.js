023c3d4e1efe166f6c13366ee493939db398ba6e
Jonathan <mohrjonathan09@gmail.com>
2024-02-12 15:42:36 +0100

    c

--- create/create.js
+++ create/create.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,6 +18,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
 let quizID = "123";
 let quiz = {};
@@ -92,3 +93,9 @@ window.clearQuestions = function() {
     } catch(e){}
     currentQuestion = 1;
 }
+
+await onAuthStateChanged(auth, (user) => {
+    if (!user) { 
+        document.getElementById("createQuiz").innerHTML = "";
+    }
+});
