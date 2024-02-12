385b4639b8abd1cb926d9e6c78d27cacc8d6b653
Jonathan <mohrjonathan09@gmail.com>
2024-02-12 11:22:49 +0100

    c

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ await onAuthStateChanged(auth, (user) => {
         document.getElementById("Account").innerHTML = `<a href="./Account/user/?id=${user.uid}">${user["displayName"]}</a>`;
         document.getElementById("create").innerHTML = `<a href="./create/index.html"><button id="createBtn">Create Quiz</button></a>`;
     } else {
-        document.getElementById("create").innerHTML = `<a><button id="createBtn">Create User to create a Quiz</button></a>`;
+        document.getElementById("create").innerHTML = `<a><button id="createBtn" style="font-size: 2vh;">Create User to create a Quiz</button></a>`;
         document.getElementById("Account").innerHTML = `<a style="color:rgb(65, 182, 197)" href="./Account/index.html">Sign-in/Sign-up</a>`;
     }
 });
